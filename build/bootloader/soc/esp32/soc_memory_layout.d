ca57d1cd42017c0aafc6a694f28085102a01bc0f
pilnikov <pilnikovav@yandex.ru>
2018-09-17 23:33:39 +0500

    Update to 1.3R3

--- build/bootloader/soc/esp32/soc_memory_layout.d
+++
@@ -1,3 +0,0 @@
-esp32/soc_memory_layout.o: \
- Z:/esp/esp-idf/components/soc/esp32/soc_memory_layout.c
-/z/esp/esp-idf/components/soc/esp32/soc_memory_layout.c:
--- build/main/cencode.d
+++
@@ -1,5 +0,0 @@
-cencode.o: Z:/msys32/home/Alex/ttgoka/main/cencode.c \
- Z:/msys32/home/Alex/ttgoka/main/include/cencode_inc.h
-
-Z:/msys32/home/Alex/ttgoka/main/include/cencode_inc.h:
-/home/Alex/ttgoka/main/./cencode.c:
