5b0cfeff02b64631442119762b88b8c2f4985f58
pilnikov <pilnikov@yandex.ru>
2019-11-06 15:31:00 +0500

    bin, test & playlist upd

--- Test/Test_vs1053/Test_vs1053.ino
+++ Test/Test_vs1053/Test_vs1053.ino
@@ -14,9 +14,9 @@
 #define VS1053_RST    12
 
 // Настройки сети WiFi
-String ssid =     "yourSSID";
-String password = "yourPASS";
-
+String ssid =     "ssid";
+String password = "pass";
+uint8_t myToneVals[4] = {15, 0, 15, 15};
 int volume = 10; // Громкость
 
 VS1053 mp3(VS1053_CS, VS1053_DCS, VS1053_DREQ);
@@ -34,22 +34,43 @@ void setup() {
   Serial.print("Start VS...");
 
   // Сброс вээски
-  pinMode(VS1053_RST, OUTPUT); 
+  pinMode(VS1053_RST, OUTPUT);
   digitalWrite(VS1053_RST, LOW);
   delay (200);
   digitalWrite(VS1053_RST, HIGH);
 
-  mp3.begin();
-  mp3.setVolume(volume);
-
-  // ------------ Тут надо выбрать один из потоков или добавить свой
+  Serial.print("\n START... \n");
   
+  mp3.begin();
+  
+  Serial.print("\n SET VOLUME... \n");
+  
+  mp3.setVolume(volume);
+  
+  Serial.print("\n PRINT VERSION... \n");
+  
+  mp3.printVersion();
+  
+  Serial.print("\n PRINT DETAILS... \n");
+  
+  mp3.printDetails();
+  
+  Serial.print("\n SET TONE... \n");
+  
+  mp3.setTone(myToneVals);
+  
+  Serial.print("\n START PIPELINE... \n");
+  
+  // ------------ Тут надо выбрать один из потоков или добавить свой
+
   //mp3.connecttohost("streambbr.ir-media-tec.com/berlin/mp3-128/vtuner_web_mp3/");
   //mp3.connecttohost("stream.landeswelle.de/lwt/mp3-192"); // mp3 192kb/s
   //mp3.connecttohost("listen.ai-radio.org:8000/320.ogg?cc=DE&now=1511557873.987&");  // ogg
   //mp3.connecttohost("tophits.radiomonster.fm/320.mp3");  //bitrate 320k
   //mp3.connecttohost("hellwegradiowest.radiovonhier.de/high/stream.mp3"); // Transfer Encoding: chunked
   mp3.connecttohost("http://ic6.101.ru:8000/p429088");
+  //mp3.connecttohost("http://ic7.101.ru:8000/a161");
+
   //mp3.connecttohost("https://icecast.omroepvenray.nl/lov.mp3"); // ssl
   //mp3.connecttoSD("320k_test.mp3"); // SD card
   //mp3.connecttospeech("Wenn die Hunde schlafen, kann der Wolf gut Schafe stehlen.", "de");
@@ -61,44 +82,7 @@ void loop()
   mp3.loop();
 }
 
-// next code is optional: (Это опции их можно в loop подключать для получения доп инфы)
 void vs1053_info(const char *info) {                // called from vs1053
-  Serial.print("DEBUG:        ");
-  Serial.println(info);                           // debug infos
-}
-void vs1053_showstation(const char *info) {         // called from vs1053
-  Serial.print("STATION:      ");
-  Serial.println(info);                           // Show station name
-}
-void vs1053_showstreamtitle(const char *info) {     // called from vs1053
-  Serial.print("STREAMTITLE:  ");
-  Serial.println(info);                           // Show title
-}
-void vs1053_showstreaminfo(const char *info) {      // called from vs1053
-  Serial.print("STREAMINFO:   ");
-  Serial.println(info);                           // Show streaminfo
-}
-void vs1053_eof_mp3(const char *info) {             // called from vs1053
-  Serial.print("vs1053_eof:   ");
-  Serial.print(info);                             // end of mp3 file (filename)
-}
-void vs1053_bitrate(const char *br) {               // called from vs1053
-  Serial.print("BITRATE:      ");
-  Serial.println(String(br) + "kBit/s");          // bitrate of current stream
-}
-void vs1053_commercial(const char *info) {          // called from vs1053
-  Serial.print("ADVERTISING:  ");
-  Serial.println(String(info) + "sec");           // info is the duration of advertising
-}
-void vs1053_icyurl(const char *info) {              // called from vs1053
-  Serial.print("Homepage:     ");
-  Serial.println(info);                           // info contains the URL
-}
-void vs1053_eof_speech(const char *info) {          // called from vs1053
-  Serial.print("end of speech:");
-  Serial.println(info);
-}
-void vs1053_lasthost(const char *info) {            // really connected URL
-  Serial.print("lastURL:      ");
-  Serial.println(info);
+    Serial.print("DEBUG:       ");
+    Serial.println(info);                             // debug infos
 }
