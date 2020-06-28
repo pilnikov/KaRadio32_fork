308b88b03599078977dd6053c60593fee03446a4
pilnikov <pilnikovav@yandex.ru>
2020-06-28 23:01:06 +0500

    upd boards

--- boards/gen.sh
+++ boards/gen.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 filename=$(basename "$1")
 fname="${filename%.*}"
-echo python ./gen.py ${fname}.csv build/${fname}.bin 0x2000
-python ./gen.py ${fname}.csv build/${fname}.bin 0x2000
-python ./esptool.py --chip esp32 --port com5 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x3a2000 build/${fname}.bin
+echo python ./gen.py --input ${fname}.csv --output build/${fname}.bin --size 0x3000 --version v1
+python ./gen.py --input ${fname}.csv --output build/${fname}.bin --size 0x3000 --version v1
+echo write flash....
+python ./esptool.py --chip esp32 --port com17 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x3a2000 build/${fname}.bin
 echo done
