bf130aac190303a3ee85e83fb8acc3e2362d4e2f
pilnikov <pilnikovav@yandex.ru>
2020-08-10 00:02:30 +0500

    add radiva 835

--- boards/gen.sh
+++ boards/gen.sh
@@ -4,5 +4,5 @@ fname="${filename%.*}"
 echo python ./gen.py --input ${fname}.csv --output build/${fname}.bin --size 0x3000 --version v1
 python ./gen.py --input ${fname}.csv --output build/${fname}.bin --size 0x3000 --version v1
 echo write flash....
-python ./esptool.py --chip esp32 --port com17 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x3a2000 build/${fname}.bin
+python ./esptool.py --chip esp32 --port com5 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x3a2000 build/${fname}.bin
 echo done
